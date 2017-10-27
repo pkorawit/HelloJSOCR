6d2e0a6679e8bb53e84114e1f505f6adbae916e7
Korawit Prutsachainimmit <korawit@Korawits-MacBook-Pro.local>
2017-10-27 21:45:53 +0700

    Formatting

--- app.js
+++ app.js
@@ -1,6 +1,5 @@
 var Tesseract = require('tesseract.js')
 var filename = 'lgd.png'
-
 ocr.recognize(filename, {lang: "eng"})
   .progress(function  (p) { console.log('progress', p)  })
   .catch(err => console.error(err))
